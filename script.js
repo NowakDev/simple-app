d41e6d88629967a15175daac27c8993a34c2c4a9
Kamil Nowak <it.kamilnowak@gmail.com>
2019-08-22 10:15:32 +0200

    fizzbuzz

---
+++ script.js
@@ -0,0 +1,14 @@
+const x = (number) => {
+  for (let i = 0; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz')
+    } else if (i % 5 === 0) {
+      console.log('buzz')
+    } else if (i % 3 === 0) {
+      console.log('fizz')
+    } else {
+      console.log(i)
+    }
+  }
+}
+
