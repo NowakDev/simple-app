a7c89cd0da67f959afe2ab924995f6a2709b76d1
Kamil Nowak <it.kamilnowak@gmail.com>
2019-08-22 10:31:38 +0200

    simple sum

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const x = (number) => {
-  for (let i = 0; i <= number; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log('fizzbuzz')
-    } else if (i % 5 === 0) {
-      console.log('buzz')
-    } else if (i % 3 === 0) {
-      console.log('fizz')
-    } else {
-      console.log(i)
-    }
-  }
-}
-
+const a = (number) => (number1) => number + number1
